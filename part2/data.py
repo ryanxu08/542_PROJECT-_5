339b52588daff9dc88d4e3fa410ebb69d954ce0a
Pei jie Xu <peijiexu@Peis-MacBook-Pro-2.local>
2018-11-04 12:17:12 -0500

    part2_completed, but need modify

--- part2/data.py
+++ part2/data.py
@@ -1,28 +1,29 @@
 import torch
 from torchnlp.datasets import penn_treebank_dataset as ptb
 from torchnlp.text_encoders import IdentityEncoder
-from torchnlp.samplers import BPTTBatchSampler
 
 # load_data
 
 
-class load_data(object):
+class Load_data(object):
     def __init__(self,is_char = False):
+        self.mapped_data = dict()
         if not is_char:
             self.train, self.valid, self.test = ptb(train=True, dev = True, test=True)
         else:
             self.train, self.valid, self.test = ptb(train=True, dev = True, test=True, train_filename="ptb.char.train.txt",
                                                     dev_filename = "ptb.char.valid.txt",test_filename="ptb.char.test.txt")
-        self.train_data = self._encode_data(self.train)
-        self.valid_data = self._encode_data(self.valid)
-        self.test_data  = self._encode_data(self.test)
-    def _encode_data(self, input):
-        data = dict()
+        self._map_data(self.train + self.valid + self.test)
+        #encodeer data
+        encoder = IdentityEncoder(self.train + self.valid + self.test)
+        self.train = torch.LongTensor(encoder.encode(self.train))
+        self.valid = torch.LongTensor(encoder.encode(self.valid))
+        self.test = torch.LongTensor(encoder.encode(self.test))
+        self.ntoken = encoder.vocab_size
+        print('hello')
+
+    def _map_data(self, input):
         for word in input:
-            if word not in data.keys():
-                data[word] = len(data)
-        idx = torch.LongTensor(len(input))
-        for index, word  in enumerate(input):
-            idx[index] = data[word]
-        return idx
+            if word not in self.mapped_data.keys():
+                self.mapped_data[word] = len(self.mapped_data.keys()) +5
 
